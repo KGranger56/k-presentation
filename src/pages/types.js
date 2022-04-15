00e4e6da2a2b6e4627c3e71fecd21802f58c9a27
KGranger56 <kelly.granger56@gmail.com>
2022-04-15 14:12:02 -0500

    Skeleton information added. Basic formatting.

--- src/pages/types.js
+++ src/pages/types.js
@@ -2,8 +2,82 @@ import React from "react";
 
 const Types = () => {
   return (
-    <div>
-      <h1>This is the Types page.</h1>
+    <div className="types">
+      <div className="block-1 ">
+        <h1>Types of Climbing</h1>
+        <ul className="jump-to">
+          <li>
+            <a href="/" title="top-rope">
+              top-rope
+            </a>
+          </li>
+          <li>
+            <a href="/" title="Top Rope">
+              Top Rope
+            </a>
+          </li>
+          <li>
+            <a href="/" title="Lead">
+              Lead
+            </a>
+          </li>
+          <li>
+            <a href="/" title="Trad/Sport">
+              Trad/Sport
+            </a>
+          </li>
+          <li>
+            <a href="/" title="Soloing/Scrambeling">
+              Not Reccomended
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="block-2">
+        <div className="box-1">
+          <div className="bouldering">
+            <h3>Bouldering</h3>
+            <p>Information about Bouldering</p>
+            <div className="back-to-top">Back to top</div>
+          </div>
+        </div>
+      </div>
+      <div className="block-3">
+        <div className="box-1">
+          <div className="top-rope">
+            <h3>Top Rope</h3>
+            <p>Information about top-rope</p>
+            <div className="back-to-top">Back to top</div>
+          </div>
+        </div>
+      </div>
+      <div className="block-4">
+        <div className="box-1">
+          <div className="lead">
+            <h3>Lead</h3>
+            <p>Information about Lead</p>
+            <div className="back-to-top">Back to top</div>
+          </div>
+        </div>
+      </div>
+      <div className="block-6">
+        <div className="box-1">
+          <div className="trad-sport">
+            <h3>Trad/Sport</h3>
+            <p>Information about Trad/Sport</p>
+            <div className="back-to-top">Back to top</div>
+          </div>
+        </div>
+      </div>
+      <div className="block-5">
+        <div className="box-1">
+          <div className="soloing">
+            <h3>Soloing/Scrambeling</h3>
+            <p>Information about soloing</p>
+            <div className="back-to-top">Back to top</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
